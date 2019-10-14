bdc4dc28abce30df2ab9fa4314cb62cdce660345
martinspilak <martinecko24@gmail.com>
2019-10-14 09:28:12 +0200

    open-lab-01.5

---
+++ open-lab-01.5/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace open_lab_01._5
+{
+    class Program
+    {
+        public static bool reverse (bool a)
+        {
+            return !a;
+        }
+        static void Main(string[] args)
+        {
+            bool a = false;
+            Console.WriteLine(reverse(a));
+        }
+    }
+}
